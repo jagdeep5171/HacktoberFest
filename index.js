e2245ab40ad4f3d4a31bfc8ed53156c4588872db
jagdeep5171 <jagdeep5171@gmail.com>
2020-10-04 02:39:28 +0530

    boilerplate ready

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const bcrypt = require('bcrypt');
 const { prototype } = require('module');
 
 app = express();
-
-
+app.use('/login', require('./login.js'));
+app.use((req,res)=>{res.send("HELLO")});
 app.listen (3000||port.env);
