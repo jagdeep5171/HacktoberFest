3d1b65e1c6aaf1d990adb7ead0c4ac74b0fc01d9
jagdeep5171 <jagdeep5171@gmail.com>
2020-10-04 03:12:55 +0530

    added signup form,signup get route, signup post route

--- signup.js
+++ signup.js
@@ -1,3 +1,11 @@
-module.exports = (req,res)=>{
+const express = require('express');
+const router = express.Router();
 
-}
+router.get('/', (req,res,next)=>{
+    res.send('<html><h1>SignUP</h1> <form action="/signup" method="POST"><div><label for="username">username</label><input type="text" name="username" id="username"></div><div><label for="password">Password</label><input type="password" name="password" id="password"></div><button class="btn" type="submit">Signup</button></form> </hmtl>')
+});
+
+router.post('/',(req,res)=>{
+    res.send('will handle the post request to signup soon')
+})
+module.exports = router;
