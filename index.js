ac608020e4be67208863e9cdc8a3c8d92d4d2a65
jagdeep5171 <jagdeep5171@gmail.com>
2020-10-04 02:25:47 +0530

    starting project;

---
+++ index.js
@@ -0,0 +1,10 @@
+const crypto = require('crypto');
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const { prototype } = require('module');
+
+app = express();
+
+
+app.listen (3000||port.env);
