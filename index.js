a15ccace0a0ff155c26b9718a88d4069f71db7fd
Alain Picard <46497761+DevCoder0x0@users.noreply.github.com>
2020-10-03 14:10:55 -0700

    Update index.js
    Declared app variable.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const { prototype } = require('module');
 
-app = express();
+const app = express();
 
 
 app.listen (3000||port.env);
